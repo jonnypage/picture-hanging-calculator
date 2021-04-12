d3066a73dcba665a85acc03534629edac377dcdd
Jon Page <me@jonnypage.ca>
2021-04-12 09:14:37 -0600

    Picture hanging logic

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,78 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div>
+      <label for="wallWidth">Wall Width</label>
+      <input
+        type="number"
+        id="wallWidth"
+        name="wallWidth"
+        v-model.number="wallWidth"
+        v-on:change="computeWall"
+      />
+    </div>
+    <div>
+      <label for="pictureCount">Picture Count</label>
+      <input
+        type="number"
+        id="pictureCount"
+        name="pictureCount"
+        v-model.number="pictureCount"
+        v-on:change="computeWall"
+      />
+    </div>
+    <div>
+      <label for="pictureWidth">Picture Width</label>
+      <input
+        type="number"
+        id="pictureWidth"
+        name="pictureWidth"
+        v-model.number="pictureWidth"
+        v-on:change="computeWall"
+      />
+    </div>
+    <div>
+      <div>Space Between Pictures: {{ spaceBetweenPictures }} cm</div>
+      <div>
+        Space Between Picture centers: {{ spaceBetweenPictureCenters }} cm
+      </div>
+      <div>
+        Space From Wall To First Picture Center:
+        {{ spaceFromWallToFirstPictureCenter }} cm
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  methods: {
+    computeWall() {
+      this.spaceBetweenPictures =
+        (this.wallWidth - this.pictureCount * this.pictureWidth) /
+        (this.pictureCount + 1);
+      this.spaceBetweenPictureCenters =
+        this.spaceBetweenPictures + this.pictureWidth;
+      this.spaceFromWallToFirstPictureCenter =
+        this.spaceBetweenPictures + this.pictureWidth / 2;
+    },
+  },
+  data() {
+    return {
+      wallWidth: 203,
+      spaceBetweenPictures: 0,
+      spaceBetweenPictureCenters: 0,
+      spaceFromWallToFirstPictureCenter: 0,
+      spaceToWall: 0,
+      pictureCount: 3,
+      pictureWidth: 33.5,
+      pictures: [{ pictureWidth: 0 }],
+    };
+  },
+  computed: {},
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
